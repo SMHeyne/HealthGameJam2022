acbcda45fdae0f8806cab066df4927530574d6ea
SMHeyne <hello@smheyne.de>
2022-05-22 16:08:04 +0200

    added a title scene
    + some more images

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchToGame()
+    {
+        SceneManager.LoadScene("YourUniverse");
+    }
+}
